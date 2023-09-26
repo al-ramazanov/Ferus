1e2c1986bfe1ccfd2b907c9bbc2a780b77cf3c8d
Ali <jipper114@gmail.com>
2023-09-26 13:23:12 +0300

    добавил поведение для каталога

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,29 +1,52 @@
-// let tl = gsap.timeline({
-// 	scrollTrigger: {
-// 		pin: ".site-container",
-// 		scrub: true,
-// 		pinSpacing: false,
-// 	}
-// })
 
-// tl.to(".first", {
-// 	scale: 0.982,
-// 	opacity: .6,
-// 	start: top, top,
-// })
-
-// tl.to(".wiper-container", {
-// 	yPercent: -100,
-// 	ease: "none",
-// 	start: top, top,
-// }),
-
-// 	tl.to(".wiper", {
-// 		yPercent: -100,
-// 		ease: "none",
-// 	})
-
-// let tl2 = gsap.timeline()
+document.addEventListener('DOMContentLoaded', () => {
+	let tl = gsap.timeline()
 
 
-// tl2.fromTo(".text", { y: 100, opacity: .5, }, { y: 0, opacity: 1, duration: 1, }) 
+	tl.fromTo(".text", { y: 100, opacity: 0, }, { delay: 1, y: 0, opacity: 1, duration: 1, })
+
+	tl.to(".catalog-hero", {
+
+		scrollTrigger: {
+			pin: ".catalog-hero",
+			scrub: true,
+			pinSpacing: false,
+		},
+		scale: 0.982,
+		opacity: 0,
+		start: top, top,
+	})
+
+})
+
+
+
+
+let tl = gsap.timeline({
+
+	scrollTrigger: {
+		pin: ".site-container",
+		scrub: true,
+		pinSpacing: false,
+	},
+
+})
+
+tl.to(".first", {
+	scale: 0.982,
+	opacity: .6,
+	start: top, top,
+})
+
+tl.to(".wiper-container", {
+	yPercent: -100,
+	ease: "none",
+	start: top, top,
+}),
+
+	tl.to(".wiper", {
+		yPercent: -100,
+		ease: "none",
+	})
+
+
